01fed01b4bac9979d804453b100b9fa3b6cd3c78
Determinant <tederminant@gmail.com>
2020-12-22 22:12:52 -0500

    upgrade toolchain

--- env.sh
+++ env.sh
@@ -2,5 +2,5 @@
 
 basedir="$(dirname $(realpath "${BASH_SOURCE[0]}"))"
 #export ESP_HOME=/opt/esp-open-sdk/
-export ESP_HOME="$basedir/esp-open-sdk/"
+export ESP_HOME="$basedir/toolchain"
 export SMING_HOME="$basedir/Sming/Sming"
--- setup.sh
+++ setup.sh
@@ -2,25 +2,43 @@
 basedir="$(dirname $(realpath "${BASH_SOURCE[0]}"))"
 arch="$(uname -m)"
 kernel="$(uname -s)"
-if [[ "$arch" != x86_64 || "$kernel" != "Linux" ]]; then
+git submodule update --init --recursive
+cd "$basedir"
+if [[ -f toolchain || -d toolchain ]]; then
+    echo "toolchain already exists, please remove it before running this script"
+    exit 1
+fi
+if [[ "$arch" == x86_64 && "$kernel" == "Linux" ]]; then
+
+# NOTE: this esp-open-sdk is too old...
+false && {
+    # clone SDK
+    git clone --recursive https://github.com/pfalcon/esp-open-sdk.git
+    # download toolchain
+    #curl -s 'https://drive.google.com/u/0/uc?export=download&confirm=Sdn9&id=1oyGELMDvFXwSt8o_CQn411bgDQGQNwPA' esp-toolchain.zip
+    esp_tc_id=1oyGELMDvFXwSt8o_CQn411bgDQGQNwPA
+    esp_tc_zip=esp_toolchain.zip
+    curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${esp_tc_id}" > /dev/null
+    curl -Lb ./cookie.txt "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie.txt`&id=${esp_tc_id}" -o ${esp_tc_zip}
+    unzip "$esp_tc_zip"
+    mv opt/esp-open-sdk/xtensa-lx106-elf toolchain/
+    rm -rf opt/
+    rm -f "$esp_tc_zip"
+}
+
+esp_tc_tar=esp_toolchain.tar.gz
+mkdir -p toolchain
+curl -sL "https://github.com/SmingHub/SmingTools/releases/download/1.0/x86_64-linux-gnu.xtensa-lx106-elf-e6a192b.201211.tar.gz" -o "$esp_tc_tar"
+tar xvf "$esp_tc_tar" -C toolchain
+
+elif [[ "$arch" == x86_64 && "$kernel" == "Darwin" ]]; then
+
+esp_tc_tar=esp_toolchain.tar.gz
+mkdir -p toolchain
+curl -sL "https://github.com/SmingHub/SmingTools/releases/download/1.0/x86_64-apple-darwin14.xtensa-lx106-elf-e6a192b.201211.tar.gz" -o "$esp_tc_tar"
+tar xvf "$esp_tc_tar" -C toolchain
+
+else
     echo "unsupported platform: arch: $arch, kernel: $kernel"
     exit 1
 fi
-git submodule update --init --recursive
-cd "$basedir"
-if [[ -f esp-open-sdk || -d esp-open-sdk ]]; then
-    echo "esp-open-sdk already exists, please remove it before running this script"
-    exit 1
-fi
-# clone SDK
-git clone --recursive https://github.com/pfalcon/esp-open-sdk.git
-# download toolchain
-#curl -s 'https://drive.google.com/u/0/uc?export=download&confirm=Sdn9&id=1oyGELMDvFXwSt8o_CQn411bgDQGQNwPA' esp-toolchain.zip
-esp_tc_id=1oyGELMDvFXwSt8o_CQn411bgDQGQNwPA
-esp_tc_zip=esp_toolchain.zip
-curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${esp_tc_id}" > /dev/null
-curl -Lb ./cookie.txt "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie.txt`&id=${esp_tc_id}" -o ${esp_tc_zip}
-unzip "$esp_tc_zip"
-mv opt/esp-open-sdk/xtensa-lx106-elf esp-open-sdk/
-rm -rf opt/
-rm -f "$esp_tc_zip"
